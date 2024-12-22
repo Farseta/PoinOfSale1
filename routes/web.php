94d281a38006b1f9f3968562d74df8793c17e5d9
farseta <rizamcshane@gmail.com>
2024-12-23 00:48:28 +0700

    adding login and register(blom dirubah)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// route::resource('', App\Http\Controllers\HomeController::class);
+
+// Route::group(['middleware' => ['auth']], function() {
+//     // Route::resource('roles', RoleController::class);
+//     // Route::resource('users', UserController::class);
+//     // Route::resource('products', ProductController::class);
+// });
